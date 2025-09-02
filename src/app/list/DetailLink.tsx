f9ca4011b52f7d60daab0972cf41684c93ee2460
seonwoo6752 <seonwoo6752@gmail.com>
2025-09-02 18:00:42 +0900

    write post

--- src/app/list/DetailLink.tsx
+++ src/app/list/DetailLink.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from "next/navigation";
 
 export default function DetailLink(){
-    let router =useRouter();
+    let router = useRouter();
     return (
         <button onClick={()=>{router.push('/list')}}>버튼</button>
     )
